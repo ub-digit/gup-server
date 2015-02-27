4df9264849eef381fa8a1f32fb5da0e46a26f164
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2015-02-27 14:09:39 +0100

    GUP-6 GUP-27 Updating users now in API and tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    user = User.find_by_id(params[:id])
+    if user
+      if user.update_attributes(user_params)
+        render json: {user: user}
+      else
+        render json: {error: user.errors }, status: 422
+      end
+    else
+      render json: {error: "Not Found"}, status: 404
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :first_name, :last_name, :role)
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,10 +7,36 @@ RSpec.describe UsersController, :type => :controller do
       expect(json).to have_key("user")
       expect(json["user"]).to have_key("id")
       expect(json["user"]["id"]).to be_present
+      expect(json).to_not have_key("error")
     end
 
     it "should fail on missing parameters" do 
       post :create, user: { first_name: "Test", last_name: "User", role: "ADMIN"}
+      expect(response.status).to eq(422)
+      expect(json).to_not have_key("user")
+      expect(json).to have_key("error")
+    end
+  end
+
+  describe "update" do
+    before :each do
+      @user = User.create(username: "testuser", first_name: "Test", last_name: "User", role: "ADMIN")
+    end
+
+    it "should update an existing user object" do
+      post :update, id: @user.id, user: {first_name: "TestNew" }
+      expect(json).to have_key("user")
+      expect(json["user"]["first_name"]).to eq("TestNew")
+      expect(json).to_not have_key("error")
+    end
+
+    it "should give 404 for a non-existing user object" do
+      post :update, id: 999999999999, user: {first_name: "TestNew" }
+      expect(response.status).to eq(404)
+    end
+ 
+    it "should give error for setting bad value on existing user object" do
+      post :update, id: @user.id, user: {role: "ADMINXX" }
       expect(response.status).to eq(422)
       expect(json).to_not have_key("user")
       expect(json).to have_key("error")
