10ed95fde4d29657f68be0408462d0b8498f7ca3
Per Larsson <per.larsson@ub.gu.se>
2015-03-09 14:32:59 +0100

    Added person presentation string.

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,4 +3,12 @@ require 'active_resource'
 class Person < ActiveResource::Base
   self.site = Rails.application.config.services[:people][:site]
   self.element_name = "person"
+
+  def presentation_string
+    "#{first_name} #{last_name}".strip
+  end
+  
+  def as_json(options = {})
+    super(methods: [:presentation_string])
+  end
 end
