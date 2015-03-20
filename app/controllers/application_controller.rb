4caebad07ba122a21f5a38f3ad6fd92d20ea3710
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2015-03-20 13:37:56 +0100

    Cleaning up logging a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,25 +8,21 @@ class ApplicationController < ActionController::Base
   # Validates token and sets user if token if valid
   def validate_token
     return if @current_user
-    logger.info "******************* HEJ HEJ **********************"
     token = get_token
-    logger.info token.inspect
     token.force_encoding('utf-8') if token
     token_object = AccessToken.find_by_token(token)
-    logger.info token_object
     if token_object && token_object.validated?
       @current_user = token_object.user
-      logger.info "token valid"
+      logger.info "ApplicationController.validate_token() => \"token valid\""
     else
       @current_user = User.new(username: 'api', role: 'USER')
-      logger.info "token invalid"
+      logger.info "ApplicationController.validate_token() => \"token invalid\""
     end
   end
 
   def get_token
     return nil if !request || !request.headers
     token_response = request.headers['Authorization']
-    logger.info token_response.inspect
     return nil if !token_response
     token_response[/^Token (.*)/,1]
   end
