65a371f39dfd804cd246c8dbe5004d4c3b283cae
Per Larsson <per.larsson@ub.gu.se>
2015-05-29 11:49:30 +0200

    Removed create publication types in seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,44 +13,12 @@ rescue ActiveRecord::StatementInvalid
 end
 
 
-def create_publication_type(label, code, content_type, template, id = nil)
-  pt=PublicationType.new(id: id, publication_type_code: code, 
-                         content_type: content_type, form_template: template, label: label)
-  pt.save(:validate=>false)
-end
-
 def create_department(id:, text:)
   department = Department.new(id: id.to_i, name: text)
   department.save
 end
 
 setup_extra_db
-
-create_publication_type('none', '- Välj -', '- Välj -', 'none', 0)
-create_publication_type('article-ref' , 'article', 'ref', 'article-ref')
-create_publication_type('article-ovr' , 'article', 'ovr', 'article-ref')
-create_publication_type('article-for' , 'article', 'for', 'article-ref')
-create_publication_type('article-pop' , 'article', 'pop', 'article')
-create_publication_type('article-rec' , 'article', 'rec', 'article')
-create_publication_type('book-ref' , 'book', 'ref', 'book')
-create_publication_type('book-nap' , 'book', 'nap', 'book')
-create_publication_type('book-edited-ref' , 'book, edited', 'ref', 'book-edited')
-create_publication_type('book-edited-nap' , 'book, edited', 'nap', 'book-edited')
-create_publication_type('textbook-nap' , 'textbook', 'nap', 'book')
-create_publication_type('report-nap' , 'report', 'nap', 'book')
-create_publication_type('book-chapter-ref' , 'book chapter', 'ref', 'book-chapter')
-create_publication_type('book-chapter-nap' , 'book chapter', 'nap', 'book-chapter')
-create_publication_type('conference-proceeding-ref' , 'conference proceeding', 'ref', 'conference-proc')
-create_publication_type('conference-proceeding-abs' , 'conference proceeding', 'abs', 'conference-proc')
-create_publication_type('conference-proceeding-pos' , 'conference proceeding', 'pos', 'conference-proc')
-create_publication_type('conference-proceeding-nap' , 'conference proceeding', 'nap', 'conference-proc')
-create_publication_type('artistic-work-ref' , 'artistic work', 'ref', 'artistic-work')
-create_publication_type('artistic-work-nap' , 'artistic work', 'nap', 'artistic-work')
-create_publication_type('text-critical-edition-nap' , 'text critical edition', 'nap', 'text-critical-edition')
-create_publication_type('doctoral-thesis-nap' , 'doctoral thesis', 'nap', 'thesis')
-create_publication_type('licentiate-thesis-nap' , 'licentiate thesis', 'nap', 'thesis')
-create_publication_type('patent-nap' , 'patent', 'nap', 'patent')
-create_publication_type('other-nap' , 'other', 'nap', 'other')
 
 Source.where(
     name: 'xkonto'
