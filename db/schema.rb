2be4b386b264e021eeeff06b79bc4a04b70db214
Per Larsson <per.larsson@ub.gu.se>
2015-06-03 17:28:26 +0200

    Removed attribute author from publication model. Added has many relation authors in publication model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603120418) do
+ActiveRecord::Schema.define(version: 20150603140555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,7 +79,6 @@ ActiveRecord::Schema.define(version: 20150603120418) do
 
   create_table "publications", force: :cascade do |t|
     t.text     "title"
-    t.text     "author"
     t.integer  "pubyear"
     t.text     "abstract"
     t.datetime "created_at"
