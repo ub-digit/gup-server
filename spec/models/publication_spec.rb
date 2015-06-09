8dfe1bbd98638f54686741eb0e767d6735a619db
Per Larsson <per.larsson@ub.gu.se>
2015-06-09 17:04:04 +0200

    Using factory girl in publication model tests.

--- spec/models/publication_spec.rb
+++ spec/models/publication_spec.rb
@@ -3,75 +3,107 @@ require 'rails_helper'
 RSpec.describe Publication, type: :model do
   require 'rails_helper'
 
-  describe "new" do
-    describe "when publication is a draft" do
-      it "needs pubid" do
-        p = Publication.new is_draft: true, is_deleted: false, title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
-        expect(p.save).to be_falsey
+  describe "pubid field" do
+    it { should validate_presence_of(:pubid) }
+    
+    context "for one specific pubid" do
+      it "should not allow 2 active (no deleted) items" do
+        p1 = build(:publication, pubid: 12345)  
+        expect(p1.save).to be_truthy
+        p2 = build(:deleted_publication, pubid: 12345)  
+        expect(p2.save).to be_truthy
+        p3 = build(:publication, pubid: 12345)  
+        expect(p3.save).to be_falsey      
       end
+      it "should allow saving an active item when deleted items already exist" do
+        p1 = build(:deleted_publication, pubid: 12345)  
+        expect(p1.save).to be_truthy
+        p2 = build(:deleted_publication, pubid: 12345)  
+        expect(p2.save).to be_truthy
+        p3 = build(:publication, pubid: 12345)  
+        expect(p3.save).to be_truthy
+      end
+    end 
+  end
 
+  describe "publication type field " do
+    context "unpublished publication" do
       it "does not need publication type" do
-        p = Publication.new is_draft: true, is_deleted: false, pubid: 12345, title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
+        p = build(:draft_publication, publication_type: nil) 
         expect(p.save).to be_truthy
       end
-
-      context "for any publication type" do
-        it "should create a new publication" do
-          p = Publication.new is_draft: true, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
-          expect(p.save).to be_truthy
-        end
-        it "does not need title" do
-          p = Publication.new is_draft: true, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', pubyear:2014, abstract:"This is an abstract..."
-          expect(p.save).to be_truthy
-        end
-        it "does not need pubyear" do
-          p = Publication.new is_draft: true, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", abstract:"This is an abstract..."
-          expect(p.save).to be_truthy
-        end
+    end
+    context "published publication" do 
+      it "needs publication type" do
+        p = build(:publication, publication_type: nil) 
+        expect(p.save).to be_falsey
       end
     end
-    describe "when publication is not a draft" do
-      context "publication type is nil" do
-        it "needs publication type to be other than 'none'" do
-          p = Publication.new is_draft: false, is_deleted: false, pubid: 12345,  title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
-          expect(p.save).to be_falsey
-        end
+  end
+
+  describe "title field" do
+    context "unpublished publication" do
+      it "does not title" do
+        p = build(:draft_publication, title: nil) 
+        expect(p.save).to be_truthy
       end
-      it "needs pubid" do
-        p = Publication.new is_draft: true, is_deleted: false, title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
+    end
+    context "published publication" do
+      it "needs title" do
+        p = build(:publication, title: nil) 
         expect(p.save).to be_falsey
       end
+    end
+  end
 
-      it "needs publication type" do
-        p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
+  describe "pubyear field" do
+    context "unpublished publication" do
+      it "does not need pubyear" do
+        p = build(:draft_publication, pubyear: nil) 
+        expect(p.save).to be_truthy
+      end
+    end
+    context "published publication" do
+      it "needs pubyear" do
+        p = build(:publication, pubyear: nil) 
+        expect(p.save).to be_falsey
+      end        
+      it "needs pubyear to be positive integer within reasonable limits" do
+        p = build(:publication, pubyear: 201) 
+        expect(p.save).to be_falsey
+
+        p = build(:publication, pubyear: -1) 
+        expect(p.save).to be_falsey
+
+        p = build(:publication, pubyear: "aa") 
         expect(p.save).to be_falsey
       end
+    end
+  end  
 
-      context "for any publication type" do
-        it "should create a new publication" do
-          p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", pubyear:2014, abstract:"This is an abstract...", sourcetitle: "Source title"
-          expect(p.save).to be_truthy
-        end
-        it "needs title" do
-          p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', pubyear:2014, abstract:"This is an abstract..."
-          expect(p.save).to be_falsey
-        end
-        it "needs pubyear" do
-          p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", abstract:"This is an abstract..."
-          expect(p.save).to be_falsey
-        end
-        it "needs pubyear to be positive integer within reasonable limits" do
-          p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", abstract:"This is an abstract...", pubyear:201
-          expect(p.save).to be_falsey
-          p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", abstract:"This is an abstract...", pubyear:-1
-          expect(p.save).to be_falsey
-          p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", abstract:"This is an abstract...", pubyear:"aa"
-          expect(p.save).to be_falsey
-        end
-        it "should throw errror when trying to create a publication with no-existing fields" do
-          expect {
-            Publication.new is_draft: false, 
-            is_deleted: false, 
+  describe "sourcetitle field" do
+    context "unpublished publication" do
+      it "does not need sourcetitle" do
+        p = build(:draft_publication, sourcetitle: nil) 
+        expect(p.save).to be_truthy
+      end
+    end
+    context "for publication type journal-articles" do 
+      it "needs sourcetitle" do
+        p = build(:publication, publication_type: "journal-articles", sourcetitle: nil) 
+        expect(p.save).to be_falsey
+      end
+    end
+  end
+
+
+
+
+  describe "creating with no-existing fields" do
+    it "should throw errror" do
+      expect {
+        Publication.new is_draft: false, 
+        is_deleted: false, 
             pubid: 12345,
             publication_type: 'journal-articles', 
             dummy: "Dummy", 
@@ -82,42 +114,4 @@ RSpec.describe Publication, type: :model do
 
           end
         end
-
-        context "for publication type: journal-articles" do
-          it "should create a new publication" do
-            p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", sourcetitle:"Test sourcetitle", pubyear:2014, abstract:"This is an abstract..."
-            expect(p.save).to be_truthy
-          end
-          it "needs journal" do
-            p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
-            expect(p.save).to be_falsey
-          end
-          it "needs authors" do
-            p = Publication.new is_draft: false, is_deleted: false, pubid: 12345, publication_type: 'journal-articles', title:"Test-title", pubyear:2014, abstract:"This is an abstract..."
-            expect(p.save).to be_falsey
-          end
-
-        end
-      end
-
-      context "for one pubid" do
-        it "should not allow 2 active (no deleted) items" do
-          p1 = Publication.new is_draft: true, is_deleted: false, pubid: 12345
-          expect(p1.save).to be_truthy
-          p2 = Publication.new is_draft: true, is_deleted: true, pubid: 12345
-          expect(p2.save).to be_truthy
-          p3 = Publication.new is_draft: true, is_deleted: false, pubid: 12345
-          expect(p3.save).to be_falsey
-        end
-      end
-      it "should allow saving an active item when deleted items already exist" do
-        p2 = Publication.new is_draft: true, is_deleted: true, pubid: 12345
-        expect(p2.save).to be_truthy
-        p3 = Publication.new is_draft: true, is_deleted: true, pubid: 12345
-        expect(p3.save).to be_truthy
-        p4 = Publication.new is_draft: true, is_deleted: false, pubid: 12345
-        expect(p4.save).to be_truthy
-      end
-    end
-  end
-
+end
