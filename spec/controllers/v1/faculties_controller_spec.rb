980e2f82764d93c94589537fc922601d3451c9db
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2015-12-15 13:09:22 +0100

    a little bit of testing

---
+++ spec/controllers/v1/faculties_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe V1::FacultiesController, type: :controller do
+  describe "index" do
+    it "should return a list of faculties" do    
+      get :index, api_key: @api_key
+      
+      expect(json["faculties"]).to_not be nil
+      expect(json["faculties"]).to be_an(Array)
+      expect(json["faculties"].count).to eq 10
+    end
+  end
+end
