283c753110f3412e697d9a4567dd67237097388f
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2016-04-19 13:35:56 +0200

    GUP-456 changed series_* to serie_* in series2publication migratpoi

--- db/migrate/20160315082813_create_series2publications.rb
+++ db/migrate/20160315082813_create_series2publications.rb
@@ -2,9 +2,9 @@ class CreateSeries2publications < ActiveRecord::Migration
   def change
     create_table :series2publications do |t|
     	t.integer :publication_id
-    	t.integer :series_id
-    	t.text :series_part
-    	t.integer :series_listplace
+    	t.integer :serie_id
+    	t.text :serie_part
+    	t.integer :serie_listplace
     	t.timestamps
     end
   end
