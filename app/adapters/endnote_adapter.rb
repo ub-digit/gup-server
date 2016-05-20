68d0db81f6a24db07817ff3e7856627ec51bff9a
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-05-20 12:42:26 +0200

    GUP-435 Remove last remaining legacy code blocks from publications controller, and move endnote related code to endnote adapter for future reference

--- app/adapters/endnote_adapter.rb
+++ app/adapters/endnote_adapter.rb
@@ -80,4 +80,65 @@ private
     end
     return str
   end
+
+  # Copied from Publications controller, not used at all at the moment, kept for future reference
+  def handle_file_import raw_xml
+    if raw_xml.blank?
+      error_msg(ErrorCodes::VALIDATION_ERROR, "#{I18n.t "publications.errors.no_data_in_file"}")
+      render_json
+      return
+    end
+
+    xml = Nokogiri::XML(raw_xml)
+    if !xml.errors.empty?
+      error_msg(ErrorCodes::VALIDATION_ERROR, "#{I18n.t "publications.errors.invalid_file"}", xml.errors)
+      render_json
+      return
+    end
+
+    # check versions
+    version_list = xml.search('//source-app').map do |element|
+      element.attr("version").to_f
+    end
+    version_list = version_list.select! do |version|
+      version < 8
+    end
+    if !version_list.empty?
+      error_msg(ErrorCodes::VALIDATION_ERROR, "#{I18n.t "publications.errors.unsupported_endnote_version"}")
+      render_json
+      return
+    end
+
+    record_count = 0
+    record_total = 0
+    return_pub = {}
+
+    xml.search('//xml/records/record').each do |record|
+      record_total += 1
+      params[:publication] = {}
+      endnote = Endnote.parse(record)
+      if endnote
+        params[:publication].merge!(endnote.as_json)
+      else
+        params[:publication][:title] = "[Title not found]"
+      end
+
+      create_basic_data
+      pub = Publication.new(permitted_params(params))
+      if pub.save
+        record_count += 1
+        if record_count == 1
+          return_pub = pub
+        end
+      else
+        error_msg(ErrorCodes::VALIDATION_ERROR, "#{I18n.t "publications.errors.update_error"}", pub.errors)
+        render_json
+        return
+      end
+    end
+    @response[:publication] = return_pub
+    @response[:meta] = {result: {count: record_count, total: record_total}}
+    render_json(201)
+  end
+
 end
