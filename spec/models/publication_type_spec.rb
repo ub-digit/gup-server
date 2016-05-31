843bea4e65a2475d74ee9934bc4dcba03a18ea4e
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-05-31 12:57:31 +0200

    Add publicationTYpe tests, NOT yes 100

--- spec/models/publication_type_spec.rb
+++ spec/models/publication_type_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PublicationType, :type => :model do
 
+  # RELATIONS
+  describe "relations" do
+    it {should have_many(:fields2publication_types)}
+    it {should have_many(:fields)}
+  end
+
+  # VALIDATIONS
   describe "code" do
     subject{build(:publication_type)}
     it {should validate_presence_of(:code)}
@@ -12,4 +19,48 @@ RSpec.describe PublicationType, :type => :model do
     it {should validate_inclusion_of(:ref_options).in_array(['ISREF', 'NOTREF', 'BOTH', 'NA'])}
   end
 
+  # METHODS
+  describe "as_json" do
+    it "should include certain methods" do
+      pt = build(:publication_type, code: "PubType1", ref_options: "NA")
+
+      json = pt.as_json
+
+      expect(json['name']).to eq pt.name
+      expect(json['description']).to eq pt.description
+      expect(json['ref_select_options']).to eq pt.ref_select_options
+    end
+  end
+
+  describe "name" do
+    it "should return translated name" do
+      pt = build(:publication_type, code: 'pubtype')
+
+      result = pt.name
+
+      expect(result).to eq I18n.t('publication_types.pubtype.label')
+    end
+  end
+
+  describe "description" do
+    it "should return translated description" do
+      pt = build(:publication_type, code: 'pubtype')
+
+      result = pt.description
+
+      expect(result).to eq I18n.t('publication_types.pubtype.description')
+    end
+  end
+
+  describe "active_fields" do
+    context "for a publication_version type" do
+      it "should return an array of fields" do
+        pt = build(:test_publication_type)
+
+        result = pt.active_fields.to_a
+
+        expect(result.first[:name]).to eq 'required'
+      end
+    end
+  end
 end
