b1dad7e00517d246ba502efb0c11f4461ff067bd
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-05-31 16:20:55 +0200

    AccessToken model 100% covered

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -21,6 +21,16 @@ RSpec.describe AccessToken, :type => :model do
       at = AccessToken.new(token: SecureRandom.hex, token_expire: Time.now+1.day)
       expect(at.save).to be_falsey
     end
+    
+    it "should return a user object for username only tokens" do
+      at = AccessToken.new(username: "xvalid", token: SecureRandom.hex, token_expire: Time.now+1.day)
+      expect(at.user).to be_kind_of(User)
+    end
+
+    it "should return a user object for proper user tokens" do
+      at = AccessToken.new(user_id: @user.id, token: SecureRandom.hex, token_expire: Time.now+1.day)
+      expect(at.user).to be_kind_of(User)
+    end
   end
 
   describe "generate_token" do
@@ -45,6 +55,20 @@ RSpec.describe AccessToken, :type => :model do
       user = User.new()
       at = AccessToken.generate_token(user)
       expect(at).to be_nil
+    end
+  end
+  
+  describe "validate token" do
+    context "convenience method" do
+      it "should give same answer as direct method" do
+        user = User.new(username: "testuser", role: "User")
+        at = AccessToken.generate_token(user)
+        expect(at.validated?).to eq(AccessToken.validate_token(at.token))
+
+        at = AccessToken.generate_token(user)
+        at.token_expire = DateTime.now - 2
+        expect(at.validated?).to eq(AccessToken.validate_token(at.token))
+      end
     end
   end
 end
