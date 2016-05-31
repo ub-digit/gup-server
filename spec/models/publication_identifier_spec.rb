7a29f4953ed1abded26e9b628e27cca2c45b1312
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-05-31 12:01:55 +0200

    Add tests for PublicationIdentifier model, now at 100 percent

--- spec/models/publication_identifier_spec.rb
+++ spec/models/publication_identifier_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe PublicationIdentifier, type: :model do
 
+  # RELATIONS
+  describe "relations" do
+    it {should belong_to(:publication_version)}
+  end
+
+  # VALIDATIONS
   describe "publication_version_id" do
     it {should validate_presence_of(:publication_version_id)}
   end
@@ -14,5 +20,38 @@ RSpec.describe PublicationIdentifier, type: :model do
 
   describe "identifier_value" do
     it {should validate_presence_of(:identifier_value)}
+  end
+
+  # METHODS
+  describe "get_label" do
+    context "for a configured identifier code" do
+      it "should return label" do
+        pi = build(:publication_identifier, identifier_code: 'pubmed')
+
+        result = pi.get_label
+
+        expect(result).to eq "Pubmed-ID"
+      end
+    end
+    context "for a non-configured identifier code" do
+      it "should return an error text" do
+        pi = build(:publication_identifier, identifier_code: 'nonexist')
+
+        result = pi.get_label
+
+        expect(result).to eq "MISSING: nonexist"
+      end
+    end
+  end
+
+  describe "as_json" do
+    it "should contain identifier_label" do
+      pi = build(:publication_identifier, identifier_code: 'pubmed')
+
+      json = pi.as_json
+
+      expect(json[:identifier_label]).to eq "Pubmed-ID"
+      expect(json['identifier_code']).to eq 'pubmed'
+    end
   end
 end
