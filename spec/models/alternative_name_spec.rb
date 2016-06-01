8beeaa78ba1f5cb619fbe83d33beb810342f1d02
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-06-01 09:22:47 +0200

    AlternativeName model 100% covered

--- spec/models/alternative_name_spec.rb
+++ spec/models/alternative_name_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe AlternativeName, type: :model do
-#  pending "add some examples to (or delete) #{__FILE__}"
+    before :each do
+      @person = create(:xkonto_person)
+      @altname = create(:alternative_name, person: @person)
+    end
+    
+    it "should return a normal json hash" do
+      json = AlternativeName.find_by_id(@altname.id).as_json
+      expect(json).to be_kind_of(Hash)
+      expect(json[:first_name]).to eq(@altname.first_name)
+      expect(json[:last_name]).to eq(@altname.last_name)
+    end
 end
