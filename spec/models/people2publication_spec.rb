84f5c659a0df7c8ff76ae9dc3fce4b9e619bb9e8
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-06-01 12:03:05 +0200

    People2publication model 100% covered

--- spec/models/people2publication_spec.rb
+++ spec/models/people2publication_spec.rb
@@ -1,10 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe People2publication, :type => :model do
-  describe "new" do
-      it { should validate_presence_of(:publication_version) }
-      it { should validate_presence_of(:person) }
-      it { should validate_presence_of(:position) }
-      it { should validate_uniqueness_of(:position).scoped_to(:publication_version_id) }
+  describe "relations" do
+    it { should belong_to(:publication_version)}
+    it { should belong_to(:reviewed_publication_version)}
+    it { should belong_to(:person)}
+    it { should have_many(:departments2people2publications)}
+  end
+  
+  describe "validations" do
+    it { should validate_presence_of(:publication_version) }
+    it { should validate_presence_of(:person) }
+    it { should validate_presence_of(:position) }
+    it { should validate_uniqueness_of(:position).scoped_to(:publication_version_id) }
+  end
+  
+  describe "as_json" do
+    context "without affiliations" do
+      it "should return json_data as a Hash" do
+        person = create(:xkonto_person)
+        json = create(:people2publication, person: person).as_json
+        expect(json).to be_kind_of(Hash)
+        expect(json['person_id']).to eq(person.id)
+        expect(json[:departments2people2publications]).to be_empty
+      end
+    end
+      
+    context "with affiliations" do
+      it "should include affiliations in json_data" do
+        @person1 = create(:xkonto_person, first_name: "First", year_of_birth: 1970)
+        @person2 = create(:xkonto_person, first_name: "Other", year_of_birth: 1971)
+        @xaccount = create(:xkonto_identifier, person: @person2, value: 'xother')
+        publication = create(:published_publication)
+        department = create(:department)
+        people2publication = create(:people2publication, publication_version: publication.current_version, person: @person2)
+        create(:departments2people2publication, people2publication: people2publication, department: department)
+
+        json = people2publication.as_json
+        expect(json[:departments2people2publications]).to_not be_empty
+      end
+    end
   end
 end
