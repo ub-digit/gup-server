c02b90f11d117c1bf0dff9d042788c35cc51b74e
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-06-01 10:04:20 +0200

    Source model 100% covered

--- spec/models/source_spec.rb
+++ spec/models/source_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Source, type: :model do
-   describe "name" do
+  describe "name" do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
     it { should_not allow_value(' ').for(:name) }
+  end
+  
+  describe "as_json" do
+    it "should return json_data as a Hash" do
+      source = create(:source)
+      json = source.as_json
+      expect(json[:name]).to eq(source.name)
+    end
   end
 end
