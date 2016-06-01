c231dcd03e93f34cec32774f54e68c50eabfa73e
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-06-01 10:42:52 +0200

    Add tests for Field and FIelds2publication_type map

--- spec/models/field_spec.rb
+++ spec/models/field_spec.rb
@@ -1,9 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Field, type: :model do
+  # RELATIONS
+  describe "relations" do
+    it {should have_many(:fields2publication_types)}
+    it {should have_many(:publication_types)}
+  end
+
+  # VALIDATIONS
   describe "name" do
     it {should validate_presence_of(:name)}
     it {should validate_uniqueness_of(:name)}
+  end
+
+  # METHODS
+  describe "label" do
+    it "should return a translated name" do
+      field = build(:field, name: 'testName')
+
+      expect(field.label).to eq I18n.t("fields.testName")
+    end
+  end
+
+  describe "is_array?" do
+    context "for a field included in ARRAY_FIELDS" do
+      it "should return true" do
+        field = build(:field, name: 'series')
+
+        expect(field.is_array?).to be_truthy
+      end
+    end
+    context "for a field NOT included in ARRAY_FIELDS" do
+      it "should return false" do
+        field = build(:field, name: 'title')
+
+        expect(field.is_array?).to be_falsey
+      end
+    end
   end
 end
 
--- spec/models/fields2publication_type_spec.rb
+++ spec/models/fields2publication_type_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Fields2publicationType, type: :model do
+  
+  # RELATIONS
+  describe "relations" do
+    it {should belong_to(:field)}
+    it {should belong_to(:publication_type)}
+  end
+
+  # VALIDATIONS
   describe "field" do
     it {should validate_presence_of(:field)}
   end
@@ -12,5 +20,17 @@ RSpec.describe Fields2publicationType, type: :model do
   describe "rule" do
     it {should validate_presence_of(:rule)}
     it {should validate_inclusion_of(:rule).in_array(['R', 'O'])}
+  end
+  
+  # METHODS
+  describe "as_json" do
+    it "should include field name and label" do
+      f2p = build(:fields2publication_type)
+
+      json = f2p.as_json
+
+      expect(json[:label]).to eq f2p.field.label
+      expect(json[:name]).to eq f2p.field.name
+    end
   end
 end
