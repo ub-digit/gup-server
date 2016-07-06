de45dd0e2b6948cdbec61461dd835a48c943cc3a
Per Larsson <per.larsson@ub.gu.se>
2016-07-06 14:03:20 +0200

    GUP-591 Faculties listed alphabetically

--- app/controllers/v1/faculties_controller.rb
+++ app/controllers/v1/faculties_controller.rb
@@ -3,7 +3,11 @@ class V1::FacultiesController < V1::V1Controller
   api :GET, '/faculties', 'Returns a list of all configurated faculties'
   def index
     faculties = Faculty.all
-    @response[:faculties] = faculties
+    if I18n.locale == :en
+      @response[:faculties] = faculties.order(name_en: :asc)
+    else
+      @response[:faculties] = faculties.order(name_sv: :asc)
+    end    
     render_json
   end
 end
--- spec/controllers/v1/faculties_controller_spec.rb
+++ spec/controllers/v1/faculties_controller_spec.rb
@@ -2,14 +2,48 @@ require 'rails_helper'
 
 RSpec.describe V1::FacultiesController, type: :controller do
   describe "index" do
-    it "should return a list of faculties" do    
-      create_list(:faculty, 10)
+    context "for existing faculties" do
+      it "should return a list of faculties" do    
+        create_list(:faculty, 10)
 
-      get :index, api_key: @api_key
+        get :index, api_key: @api_key
       
-      expect(json["faculties"]).to_not be nil
-      expect(json["faculties"]).to be_an(Array)
-      expect(json["faculties"].count).to eq 10
+        expect(json["faculties"]).to_not be nil
+        expect(json["faculties"]).to be_an(Array)
+        expect(json["faculties"].count).to eq 10
+      end
+      context "for locale sv" do
+        it "should return a list of faculties ordered by name_sv" do
+          create(:faculty, name_sv: "AAA", name_en: "DDD" )
+          create(:faculty, name_sv: "CCC", name_en: "BBB" )
+
+          get :index, locale: 'sv', api_key: @api_key
+
+          expect(json['faculties']).to_not be nil
+          expect(json['faculties'][0]['name']).to eq "AAA"
+          expect(json['faculties'][1]['name']).to eq "CCC"
+        end
+      end
+      context "for locale en" do
+        it "should return a list of faculties ordered by name_en" do
+          create(:faculty, name_sv: "AAA", name_en: "DDD" )
+          create(:faculty, name_sv: "CCC", name_en: "BBB" )
+
+          get :index, locale: 'en', api_key: @api_key
+
+          expect(json['faculties']).to_not be nil
+          expect(json['faculties'][0]['name']).to eq "BBB"
+          expect(json['faculties'][1]['name']).to eq "DDD"
+        end
+      end
+    end
+    context "for an empty list of faculties" do
+      it "should return an empty list" do
+        get :index, api_key: @api_key
+
+        expect(json['faculties']).to be_an(Array)
+        expect(json['faculties'].count).to eq 0
+      end
     end
   end
 end
