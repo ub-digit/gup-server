e8418918d4b09facd16f3dec047c07f51044a99b
David Gustafsson <david.gustafsson@ub.gu.se>
2016-10-20 10:52:09 +0200

    Move ControllerError to correct class

--- app/controllers/v1/v1_controller.rb
+++ app/controllers/v1/v1_controller.rb
@@ -1,3 +1,15 @@
 class V1::V1Controller < ApplicationController
+
+  class V1::ControllerError < StandardError
+    attr_reader :code
+    attr_reader :errors
+    attr_reader :message
+    def initialize(code: ErrorCodes::ERROR, errors: {}, message: '')
+      @code = code
+      @errors = errors
+      super(message)
+    end
+  end
+
   before_filter :validate_access
 end
