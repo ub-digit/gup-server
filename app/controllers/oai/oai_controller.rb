79cd320778b5a62c3e727341817389a88cc3641c
Per Larsson <per.larsson@ub.gu.se>
2016-10-27 11:24:40 +0200

    Oai Provider class is moved from environment.rb to controller.

--- app/controllers/oai/oai_controller.rb
+++ app/controllers/oai/oai_controller.rb
@@ -1,9 +1,32 @@
+require 'oai'
+
+# Define oai provider class here, it will be implemented just before it is needed for the first time. 
+class OaiProvider < OAI::Provider::Base
+end
+
 class Oai::OaiController < ApplicationController
   def index
     # Remove controller and action from the options.
     options = params.delete_if { |k,v| %w{controller action}.include?(k) }
+    setup_oai_provider
     provider = OaiProvider.new
     response =  provider.process_request(options)
     render text: response, content_type: 'text/xml'
   end
+
+private
+  # Oai Provider class will be evaluated here, only once.
+  def setup_oai_provider
+  	@@provider_set ||= false 
+  	return if @@provider_set 
+    OaiProvider.class_eval do
+      repository_name APP_CONFIG['oai_settings']['repository_name']
+      repository_url APP_CONFIG['oai_settings']['repository_url']
+      record_prefix APP_CONFIG['oai_settings']['record_prefix']
+      admin_email APP_CONFIG['oai_settings']['admin_email']
+      source_model OAI::Provider::ActiveRecordWrapper.new(Publication.where("deleted_at is null").where("published_at is not null"), {limit: 100})
+    end
+    @@provider_set = true
+  end  
+
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,4 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 
-require 'oai'
-class OaiProvider < OAI::Provider::Base
-  repository_name APP_CONFIG['oai_settings']['repository_name']
-  repository_url APP_CONFIG['oai_settings']['repository_url']
-  record_prefix APP_CONFIG['oai_settings']['record_prefix']
-  admin_email APP_CONFIG['oai_settings']['admin_email']
-  source_model OAI::Provider::ActiveRecordWrapper.new(Publication.where("deleted_at is null").where("published_at is not null"), {limit: 100})
-end
+
