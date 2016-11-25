2481e86a2c73b85b8c52ee49d6131e70b3f6a47c
Per Larsson <per.larsson@ub.gu.se>
2016-11-25 13:57:37 +0100

    MOds schema and namespace visible in ListMetadataFormat updated.

--- config/initializers/oai_mods.rb
+++ config/initializers/oai_mods.rb
@@ -6,6 +6,8 @@ module OAI::Provider::Metadata
     def initialize
       @prefix = 'mods'
       @element_namespace = 'mods'
+      @schema = 'http://www.loc.gov/standards/mods/v3/mods-3-5.xsd'
+      @namespace = 'http://www.loc.gov/mods/v3/'
     end
 
   end
