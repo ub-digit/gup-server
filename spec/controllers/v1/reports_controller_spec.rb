ff031a195a5b6da9deb4ffc86a720fdc22d5ca59
David Gustafsson <david.gustafsson@ub.gu.se>
2016-11-28 15:47:51 +0100

    Fix reports controller tests after change in return format

--- spec/controllers/v1/reports_controller_spec.rb
+++ spec/controllers/v1/reports_controller_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe V1::ReportsController, type: :controller do
       @person_a = create(:xkonto_person)
       @person_b = create(:xkonto_person)
       @person_c = create(:xkonto_person2)
-      
+
       @affiliations = [
         { person: create(:person), department: @dep_1 },
         { person: create(:person), department: @dep_1 },
@@ -49,29 +49,29 @@ RSpec.describe V1::ReportsController, type: :controller do
         { person: create(:person), department: @dep_2 },
         { person: @person_c, department: @dep_3 },
       ]
-      
+
       # Add a draft to catch that only published publications should be counted
       @draft = create(:draft_publication)
       @draft.current_version.update_attribute(:pubyear, 2010)
-      tmp = create(:people2publication, 
+      tmp = create(:people2publication,
                    person: @person_b,
                    publication_version: @draft.current_version)
       create(:departments2people2publication,
              department: @dep_3,
              people2publication: tmp)
-      
+
       # Generate affilations
-      @affiliations.each.with_index do |aff,i| 
-        tmp = create(:people2publication, 
+      @affiliations.each.with_index do |aff,i|
+        tmp = create(:people2publication,
                      person: aff[:person],
                      publication_version: @publications[i].current_version)
         create(:departments2people2publication,
                department: aff[:department],
                people2publication: tmp)
       end
-      
+
       # Need one item with multiple people affiliated
-      tmp = create(:people2publication, 
+      tmp = create(:people2publication,
                    person: @person_c,
                    publication_version: @publications[-2].current_version)
 
@@ -92,7 +92,7 @@ RSpec.describe V1::ReportsController, type: :controller do
           expect(json['report']['data'][0][0]).to eq(10)
         end
       end
-      
+
       context "filtered by pubyear" do
         it "should return count only for requested year range" do
           post :create, report: { filter: {start_year: 2010, end_year: 2012}, }, api_key: @api_key
@@ -122,7 +122,7 @@ RSpec.describe V1::ReportsController, type: :controller do
           expect(json['report']['data'][0][0]).to eq(7)
         end
       end
-      
+
       context "filtered by publication type" do
         it "should return count only for requested single pub type" do
           post :create, report: { filter: {publication_types: [@publication_type_1.id]}, }, api_key: @api_key
@@ -138,7 +138,7 @@ RSpec.describe V1::ReportsController, type: :controller do
           expect(json['report']['data'][0][0]).to eq(5)
         end
       end
-      
+
       context "filtered by content type" do
         it "should return count only for requested single ref_value" do
           post :create, report: { filter: {ref_value: true}, }, api_key: @api_key
@@ -154,7 +154,7 @@ RSpec.describe V1::ReportsController, type: :controller do
           expect(json['report']['data'][0][0]).to eq(10)
         end
       end
-      
+
       context "filtered by faculty" do
         it "should return count only for selected faculty" do
           post :create, report: { filter: {faculties: [@faculty_1]}, }, api_key: @api_key
@@ -183,7 +183,7 @@ RSpec.describe V1::ReportsController, type: :controller do
           expect(json['report']['data'][0][0]).to eq(2)
         end
       end
-      
+
       context "without filter" do
         context "grouped by year" do
           it "should return matrix with count per year" do
@@ -209,14 +209,13 @@ RSpec.describe V1::ReportsController, type: :controller do
             expect(json['report']['data'].size).to eq(8)
             expect(json['report']['data'][0][0]).to eq(2005)
             expect(json['report']['data'][5][0]).to eq(2012)
-            expect(json['report']['data'][0][1]).to eq("Artikel i vetenskaplig tidskrift")
-            expect(json['report']['data'][5][1]).to eq("Samlingsverk (red.)")
+            expect(json['report']['data'][0][1][0]).to eq("Artikel i vetenskaplig tidskrift")
+            expect(json['report']['data'][5][1][0]).to eq("Samlingsverk (red.)")
             expect(json['report']['data'][0][2]).to eq(1)
             expect(json['report']['data'][5][2]).to eq(1)
           end
         end
       end
-      
       context "with filter" do
         context "grouped by year for specific publication type" do
           it "should return matrix with count per year" do
@@ -242,13 +241,12 @@ RSpec.describe V1::ReportsController, type: :controller do
             expect(json['report']['data'].size).to eq(5)
             expect(json['report']['data'][0][0]).to eq(2010)
             expect(json['report']['data'][4][0]).to eq(2015)
-            expect(json['report']['data'][1][1]).to eq("Bok")
-            expect(json['report']['data'][4][1]).to eq("Artikel i övriga tidskrifter")
+            expect(json['report']['data'][1][1][0]).to eq("Bok")
+            expect(json['report']['data'][4][1][0]).to eq("Artikel i övriga tidskrifter")
             expect(json['report']['data'][0][2]).to eq(2)
             expect(json['report']['data'][4][2]).to eq(1)
           end
         end
-        
         context "column validation" do
           it "should give error when requesting grouping on columns that do not exist" do
             post :create, report: { columns: [:unknown_column] }, api_key: @api_key
@@ -256,7 +254,6 @@ RSpec.describe V1::ReportsController, type: :controller do
           end
         end
       end
-      
       context "export csv" do
         it "should return the report data in csv format when requested" do
           #get :show, name: "testreport", report: { filter: {start_year: 2010, end_year: 2015}, columns: [:year, :publication_type_id], }, api_key: @api_key
