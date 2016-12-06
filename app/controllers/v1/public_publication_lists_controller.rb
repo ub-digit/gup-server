8605eaf2e2759229a20909d3c04fe79645cdf8e3
Per Larsson <per.larsson@ub.gu.se>
2016-12-06 14:39:46 +0100

    Title and pubyear added as sort params in public publication list controller.

--- app/controllers/v1/public_publication_lists_controller.rb
+++ app/controllers/v1/public_publication_lists_controller.rb
@@ -5,8 +5,18 @@ class V1::PublicPublicationListsController < ApplicationController
   def index
     publications = Publication.all
     
-    # Default sort order
-    order = "publications.updated_at desc"
+    # Get sort order params
+    sort_by = params[:sort_by] || ''
+    if sort_by.eql?("pubyear")
+      order = "publication_versions.pubyear desc, publications.updated_at desc"
+    elsif sort_by.eql?("title")
+      order = "publication_versions.title asc, publications.updated_at desc"
+    else
+      # pubyear should be default sort order
+      order = "publications.updated_at desc"
+    end
+    # This join is made just for get the sort fields
+    publications = Publication.joins(:current_version)
 
     publications = publications.non_deleted.published
     @response = generic_pagination(resource: publications, resource_name: 'publications', page: params[:page], additional_order: order, options: {include_authors: true, brief: true})
