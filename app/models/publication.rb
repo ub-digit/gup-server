3ef56520af77de943b269e3dda3b006db686cab6
johan larsson <johan.larsson@ub.gu.se>
2017-01-16 15:08:47 +0100

    added comment to json-response

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -107,6 +107,8 @@ class Publication < ActiveRecord::Base
         }
       end
       result[:biblreview_postponed_until] = biblreview_postponed_until
+      result[:biblreview_postponed_comment] = biblreview_postponed_comment
+      
       result[:files] = files(current_xaccount: options[:current_xaccount])
     end
     result
@@ -161,6 +163,15 @@ class Publication < ActiveRecord::Base
     end
   end
 
+  def biblreview_postponed_comment
+    postpone_date = postpone_dates.where(deleted_at: nil).where("postponed_until > (?)", DateTime.now).first
+    if postpone_date
+      return postpone_date.comment
+    else
+      return nil
+    end
+  end
+
   # Split and build new publication and its first version
   def self.build_new(params)
     publication = Publication.new
@@ -206,7 +217,7 @@ class Publication < ActiveRecord::Base
     end
   end
 
-  def set_postponed_until(postponed_until:, postponed_by:, epub_ahead_of_print: nil)
+  def set_postponed_until(postponed_until:, postponed_by:, epub_ahead_of_print: nil, comment:nil)
     postpone_dates.each do |postpone_object|
       if !postpone_object.deleted_at
         if !postpone_object.update_attributes(deleted_at: DateTime.now, deleted_by: postponed_by)
@@ -214,7 +225,7 @@ class Publication < ActiveRecord::Base
         end
       end
     end
-    if !postpone_dates.create(postponed_until: postponed_until, created_by: postponed_by, updated_by: postponed_by)
+    if !postpone_dates.create(postponed_until: postponed_until, created_by: postponed_by, updated_by: postponed_by, comment: comment)
       return false
     end
     if epub_ahead_of_print
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124085540) do
+ActiveRecord::Schema.define(version: 20170116134021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -291,6 +291,7 @@ ActiveRecord::Schema.define(version: 20161124085540) do
     t.text     "updated_by"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "comment"
   end
 
   add_index "postpone_dates", ["deleted_at"], name: "index_postpone_dates_on_deleted_at", using: :btree
