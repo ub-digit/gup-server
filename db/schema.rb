e489912cc7245b5379042ad488f07fedbf378902
Per Larsson <per.larsson@ub.gu.se>
2017-03-24 09:01:24 +0100

    Rename column names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322170259) do
+ActiveRecord::Schema.define(version: 20170324074749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -372,8 +372,8 @@ ActiveRecord::Schema.define(version: 20170322170259) do
     t.string   "ref_options",    null: false
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
-    t.text     "name_sv"
-    t.text     "name_en"
+    t.text     "label_sv"
+    t.text     "label_en"
     t.text     "description_sv"
     t.text     "description_en"
   end
