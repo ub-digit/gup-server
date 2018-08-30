e87042debd8bab8b8b19b85b7ffc41b00e264c32
Torgny Rasmark <Torgny.Rasmark@ub.gu.se>
2018-08-30 16:12:31 +0200

    reverting

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,4 +38,6 @@ set :deploy_to, deploy_config['path']
 #end
 
 #set :branch, get_tag # Sets branch according to given tag
-set :branch, ENV['branch']
+set :branch, 'EXPERIMENT'
+#set :branch, "ENV['branch']"
+
